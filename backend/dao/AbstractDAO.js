4034c7c6ccae5a1dabd5139cc6fc1b1ea615b3c6
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-03-25 22:31:46 -0400

    Exporting AbstractDAO

--- backend/dao/AbstractDAO.js
+++ backend/dao/AbstractDAO.js
@@ -1,6 +1,11 @@
 class AbstractDAO{
+    constructor(){
+
+    }
     getChatroom(room_id){};
     createChatroom(room_id){};
     updateChatroom(chatroom){};
     deleteChatroom(room_id){};
 }
+
+module.exports = AbstractDAO;
