464f533501be3fb7c2da96c208b31447fa005f96
poorna-chander <p.poornachander@live.com>
2023-03-27 17:26:45 -0400

    backed for image classifier service

---
+++ ml_image_class_service/app.py
@@ -0,0 +1,57 @@
+# from flask import Flask, request
+# import uuid
+# from flask_cors import CORS
+# from datetime import datetime
+
+# app = Flask(__name__)
+# CORS(app)
+
+
+# @app.route('/upload-image', methods=['POST'])
+# def upload_image():
+#     if 'image' not in request.files:
+#         return 'No image provided', 400
+
+#     file = request.files['image']
+#     file.save(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + ".jpg")
+
+#     return 'Image uploaded successfully', 200
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import model
+from datetime import datetime
+import time
+import random
+import base64
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+#  async_mode='eventlet',
+
+
+@app.route('/upload-image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return 'No image provided', 400
+
+    file = request.files['image']
+    # file.save(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + ".jpg")
+    # filename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + "-" + \
+    #     str(random.randint(100000, 999999)) + ".jpg"
+    # file.save(filename)
+    socketio.emit('message', model.getPrediction(file))
+    return 'Image uploaded successfully', 200
+
+
+if __name__ == '__main__':
+    # socketio.start_background_task(send_random_message)
+    socketio.run(app, debug=True)
