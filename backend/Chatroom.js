66eb26716ff14fc6e634d762a2495260663449e1
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-03-28 17:12:33 -0400

    Improvements to Chatroom model

--- backend/Chatroom.js
+++ backend/Chatroom.js
@@ -6,7 +6,7 @@ class Chatroom{
 
     constructor(id, name){
         this.#id = id;
-        this.#messages = [];
+        this.#messages = []; 
         this.#users = new Map();
         this.#name = name;
     }
@@ -23,17 +23,43 @@ class Chatroom{
         return this.#users;
     };
 
+    get name(){
+        return this.#name;
+    }
+
+    /**
+     * Adds a message to the chatroom
+     * @param {*} message message of chatroom
+     */
     addMessage(message){
         this.#messages.push(message);
         console.log(`Added Message ${message}`);
     };
 
+    /**
+     * Adds a new user to the chatroom
+     * @param {*} user format {id: "unique name"}
+     */
     addUser(user){
+        this.#users.forEach(u=>{
+            if(u['id'] === user['id']){
+                return false;
+            }
+        });
         this.#users.set(user['id'],user);
+        return true;
     };
 
+    /**
+     * Removes a user from the chatroom
+     * @param {*} userId 
+     */
     deleteUser(userId){
-        this.#users.delete(userId);
+        if(this.#users.get(user['id'])){
+            this.#users.delete(userId);
+            return true;
+        }
+        return false;
     };
 }
 
