88a37e59feb956f4ff53890a6fb2e1aef10dc54b
jagrat7 <raojagrat@hotmail.com>
2023-03-29 01:52:48 -0400

    als changes

--- sign2textUI/src/pages/Als.tsx
+++ sign2textUI/src/pages/Als.tsx
@@ -1,47 +1,123 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container, Grid, List, ListItem, Paper, TextField, Typography } from "@mui/material";
 import Webcam from "react-webcam";
-import { useRef, useState, useCallback } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
+import { io } from "socket.io-client";
+
 
 const Als = () => {
     const videoConstraints = {
-        width: 1280,
-        height: 720,
+        width: 400,
+        height: 400,
         facingMode: "user"
     };
-    const [url, setUrl] = useState<string | null>(null);
-    const webcamRef = useRef<Webcam>(null);
-    const capture = useCallback(() => {
-        const imageSrc = webcamRef.current?.getScreenshot();
-        if (imageSrc) {
-            setUrl(imageSrc)
-            console.log(imageSrc);
 
+
+    const [messages, setMessages] = useState<string[] | []>([]);
+    const [image, setImage] = useState<string | null>(null);
+    const [url, setUrl] = useState<string | null>(null);
+    const [dimensions, setDimensions] = useState<{ width: number; height: number }>({ width: 750, height: 750 });
+
+    const webcamDimensionsRef = useRef(null);
+    const webcamRef = useRef<Webcam>(null);
+
+    const capture = useCallback(async () => {
+        const imageSrc = webcamRef.current?.getScreenshot();
+
+        if (imageSrc) {
+            setUrl(imageSrc);
+            setImage(imageSrc);
+
+            try {
+                const response = await fetch("http://localhost:5000/upload-image", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ imageSrc }),
+                });
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }, [webcamRef]);
 
 
+    // const socket = io("http://localhost:5000");
+    // useEffect(() => {
+    //     socket.on("message", (message) => {
+    //         setMessages((messages) => [...messages, message]);
+    //     });
+    // }, []);
+    const handleResize = () => {
+        setDimensions({
+            width: window.innerWidth / 1.6,
+            height: window.innerHeight / 1.6
+        });
+    };
+
+    useState(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
     return (
 
         <div className="Asl">
-            <Container maxWidth="lg" sx={{ marginTop: "3%" }}>
-                <Box>
-                    <Webcam
-                        imageSmoothing={true}
-                        audio={false}
-                        height={720}
-                        screenshotFormat="image/png"
-                        width={1280}
-                        ref={webcamRef}
-                        videoConstraints={videoConstraints}></Webcam>
-                </Box>
-                <Button sx={{ display: "flex", justifyContent: "center" }} variant="contained" onClick={capture}>Take a screenshot</Button>
+            <Container maxWidth="xl" sx={{ marginTop: "3%" }}>
+
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Box display={"flex"} justifyContent="left" sx={{ backgroundColor: "rgb(33, 33, 33)", overflow: "hidden" }}>
+                            <Webcam
+                                imageSmoothing={true}
+                                audio={false}
+                                height={dimensions.height}
+                                screenshotFormat="image/jpeg"
+                                width={dimensions.width}
+                                ref={webcamRef}
+                                videoConstraints={videoConstraints}></Webcam>
+                        </Box>
+                        <Button sx={{ marginTop: "1%" }} variant="contained" onClick={capture}>Take a screenshot</Button>
+
+                    </Grid>
+
+                    <Grid item xs={4} >
+                        <Paper elevation={8} sx={{ minHeight: window.innerHeight / 1.6 }} >
+                            <Typography variant="h2" padding={"4%"} textAlign="center">Chat</Typography>
+                            <Box display="flex" flexDirection="column" justifyContent="space-between" sx={{ height: "100%" }}>
+                                <Box>
+                                    <List >
+                                        {messages.map((message, index) => (
+                                            <ListItem key={index}>{message}</ListItem>
+                                        ))}
+                                        <ListItem>askdjlksadjlkas </ListItem>
+                                        <ListItem>askdjlksadjlkas </ListItem>
+                                        <ListItem>askdjlksadjlkas </ListItem>
+                                    </List>
+                                </Box>
+
+                                <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                                    <TextField id="outlined-basic" label="Message" sx={{ alignSelf: "end", padding: "2%", width: "80%" }} variant="outlined" />
+                                    <Button variant="contained" sx={{ alignItems: "center", alignSelf: "end" }}>send</Button>
+                                </Box>
+                            </Box>
+                        </Paper>
+                    </Grid>
+
+
+                </Grid>
+
+
                 {url && (
                     <>
-                        <div>
-                            <button onClick={() => {  setUrl(null); }}>
+                        <Box marginTop={"40%"}>
+                            <Button onClick={() => { setUrl(null); }}>
                                 delete
-                            </button>
-                        </div>
+                            </Button>
+                        </Box>
                         <div>
                             <img src={url} alt="Screenshot" />
                         </div>
