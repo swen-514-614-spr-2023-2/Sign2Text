e440dba3a86b895ecf0d1acb8ceec8e023b3522e
poorna-chander <p.poornachander@live.com>
2023-04-04 12:52:00 -0400

    image socket to rest, and backend

--- backend/ChatroomAPI.js
+++ backend/ChatroomAPI.js
@@ -1,100 +1,93 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.Server(app);
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const ChatroomService = require('./ChatroomService');
+const ChatroomService = require("./ChatroomService");
 
 const chatroomService = new ChatroomService();
 
-
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json()); // for parsing application/json
 
-
-
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-      }
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
+  socket.on("disconnect", (socket) => {
+    console.log("User disconnected");
+  });
 
-io.on('connection',(socket)=>{
-    console.log("A user connected");
-
-    socket.on('disconnect',(socket)=>{
-        console.log('User disconnected');
-    });
-
-    socket.on('chat message',(msg)=>{
-        console.log(msg);
-        if(chatroomService.sendMessage(msg)){
-            io.emit(`room#${msg['roomId']}`, msg);
-        }
-    });
-
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    if (chatroomService.sendMessage(msg)) {
+      io.emit(`room#${msg["roomId"]}`, msg);
+    }
+  });
 });
-
-
 
 //need an api method that image service will call to send text message.
+app.post("/prediction", (req, res) => {
+  console.log("Recieved new prediction");
+  const body = req.body;
+  console.log(body);
+  const pred = chatroomService.sendMessage(body);
+  res.status(200).send(body);
+});
 
 //create new room
 /**
  * Create a new room
  * body format: {name: #}
  */
-app.post('/chatroom',(req,res)=>{
-    console.log('Recieved request to create chatroom');
-    const body = req.body;
-    console.log(body);
-    const chRoomId = chatroomService.createChatroom(body['name']);
-    res.status(200).send({roomId : chRoomId});
+app.post("/chatroom", (req, res) => {
+  console.log("Recieved request to create chatroom");
+  const body = req.body;
+  console.log(body);
+  const chRoomId = chatroomService.createChatroom(body["name"]);
+  res.status(200).send({ roomId: chRoomId });
 });
 
 //delete chatroom
-app.delete('/chatroom',(req,res)=>{
-    console.log('Recieved request to delete chatroom');
-    const body = req.body;
-    console.log(`Body: ${body}`);
-    if(chatroomService.deleteChatroom(body['roomId'])){
-        res.status(200).end();
-    }
-    else res.status(409).end();
+app.delete("/chatroom", (req, res) => {
+  console.log("Recieved request to delete chatroom");
+  const body = req.body;
+  console.log(`Body: ${body}`);
+  if (chatroomService.deleteChatroom(body["roomId"])) {
+    res.status(200).end();
+  } else res.status(409).end();
 });
 
-app.get('/chatroom',(req,res)=>{
-    const allRooms = chatroomService.getAllChatrooms();
-    console.log(`In chatroom API: ${allRooms}`);
-    res.status(200).send(allRooms);
+app.get("/chatroom", (req, res) => {
+  const allRooms = chatroomService.getAllChatrooms();
+  console.log(`In chatroom API: ${allRooms}`);
+  res.status(200).send(allRooms);
 });
 
 /**
  * body format: {roomId: #, user: {id: #}}
  */
-app.put('/chatroom/users',(req,res)=>{
-    console.log('Recieved request to add user to chatroom');
-    const body = req.body;
-    if(chatroomService.enterChatroom(body['roomId'], body['user'])){
-        res.status(200).end();
-    }
-    else res.status(409).end();
-})
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+app.put("/chatroom/users", (req, res) => {
+  console.log("Recieved request to add user to chatroom");
+  const body = req.body;
+  if (chatroomService.enterChatroom(body["roomId"], body["user"])) {
+    res.status(200).end();
+  } else res.status(409).end();
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-
-
-
-server.listen(3000,()=>{
-    console.log("Listening on port 3000");
+server.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
--- ml_image_class_service_v2/app.py
+++ ml_image_class_service_v2/app.py
@@ -6,30 +6,33 @@ import base64
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from flask import Flask, request, jsonify
+import requests
 
 
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
+# socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
 #  async_mode='eventlet',
 
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
+    if 'image' not in request.form:
         return 'No image provided', 400
+    room_ = request.form['roomid']
+    file_ = request.form['image']
 
-    file = request.files['image']
-    # file.save(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + ".jpg")
-    # filename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + "-" + \
-    #     str(random.randint(100000, 999999)) + ".jpg"
-    # file.save(filename)
-    socketio.emit('message', model.getPrediction(file))
-    # socketio.emit('message', "predicted")
+    url = 'http://localhost:3000/prediction'  # replace with the URL of the other service's API endpoint
+    data = {'text': model.getPrediction(file_), 'roomid': room_}  # replace with the data you want to send in the request
+
+    response = requests.post(url, json=data)
+
+    # socketio.emit('message', model.getPrediction(file_))
+
     return 'Image uploaded successfully', 200
 
 
 if __name__ == '__main__':
     # socketio.start_background_task(send_random_message)
-    socketio.run(app,host="0.0.0.0", debug=True)
+    app.run( debug=True)
