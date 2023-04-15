9a6f7b8da93959f8bba5f4634fdeecf12073a7e1
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-04-14 20:39:19 -0400

    Changes to importing aws-sdk

--- backend/Database.js
+++ backend/Database.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+//documentation: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/index.html
+const {DynamoDBClient, ListTablesCommand} = require('@aws-sdk/client-dynamodb');
 
 
 class DatabaseConnection{
@@ -7,14 +8,15 @@ class DatabaseConnection{
 
     constructor(region='us-east-1', tableName='kafkaTopicTable'){
         AWS.config.loadFromPath('./config.json');
-        
-        this.#client = new AWS.DynamoDB({region: region});
+
+        this.#client = new DynamoDBClient({region: region});
         this.#tableName = tableName
 
     }
 
-    init(){
-
+    async init(options={}){
+        const data = await this.#client.send(new ListTablesCommand(options));
+        
     }
 
     kafkaTopicTableExists(){
