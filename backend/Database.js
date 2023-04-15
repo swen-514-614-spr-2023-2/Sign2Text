276fb5ee29badb2643eaa042fd1029240d216ad7
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-04-14 20:18:06 -0400

    config file for aws sdk

--- backend/Database.js
+++ backend/Database.js
@@ -1,16 +1,20 @@
 const AWS = require('aws-sdk');
 
+
 class DatabaseConnection{
     #client;
     #tableName;
 
-    constructor(region='us-east-1'){
-        this.#client = new AWS.DynamoDB({region});
-        this.#tableName = 'kafkaTopicTable'
+    constructor(region='us-east-1', tableName='kafkaTopicTable'){
+        AWS.config.loadFromPath('./config.json');
+        
+        this.#client = new AWS.DynamoDB({region: region});
+        this.#tableName = tableName
+
     }
 
     init(){
-        
+
     }
 
     kafkaTopicTableExists(){
