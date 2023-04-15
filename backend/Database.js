f530d97f543dfad706f020cae2465ab1d93cef72
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-04-15 15:48:06 -0400

    querying working

--- backend/Database.js
+++ backend/Database.js
@@ -1,5 +1,7 @@
 //documentation: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/index.html
-const {DynamoDBClient, ListTablesCommand} = require('@aws-sdk/client-dynamodb');
+const { DynamoDBClient, ListTablesCommand } = require('@aws-sdk/client-dynamodb');
+const AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
 
 
 class DatabaseConnection{
@@ -17,9 +19,26 @@ class DatabaseConnection{
     async init(options={}){
         const data = await this.#client.send(new ListTablesCommand(options));
         
+        
     }
 
     kafkaTopicTableExists(){
 
     }
 }
+
+async function test(){
+    const dynamodb = new AWS.DynamoDB();
+
+    // dynamodb.listTables({},(err,data)=>{
+    //     if(err) console.log(err, err.stack)
+    //     else console.log(data);
+    // });
+
+    dynamodb.deleteTable({TableName : "firstTable"},(err,data)=>{
+        if(err) console.log(err, err.stack);
+        else console.log(data);
+    })
+}
+
+test();
