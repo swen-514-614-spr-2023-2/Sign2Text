8bb30154e42000367991c6251a56967c60946638
jagrat7 <raojagrat@hotmail.com>
2023-04-23 22:41:34 -0400

    nowItShouldwork

--- sign2textUI/src/components/sd.tsx
+++ sign2textUI/src/components/sd.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Grid, List, ListItem, Paper, TextField, Typography } from "@mui/material";
 import { socket } from '../utils/socket';
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 interface ChatboxProps {
     roomid: string | undefined
     height: number
@@ -19,31 +19,18 @@ const Chatbox = ({ roomid, height }: ChatboxProps) => {
     const [Emessaages, setEmessaages] = useState<string[] | []>([])
     const [msgEvent, setMsgEvent] = useState([])
     const [emitted, setemitted] = useState(false)
-
-    const submissionBox = useRef<HTMLDivElement>(null)
-    const CBheight = useRef<HTMLDivElement>(null)
-    const [msgHeight, setmsgHeight] = useState(0)
-    const messageElm = useRef(null)
-    const [paperHeight, setpaperHeight] = useState(0)
-    const [listHeight, setlistHeight] = useState(0)
-
     function onSend(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         setIsLoading(true);
-        // console.log(textField.current.target.value)
-        if(value!=""){
+        setValue("")
         socket.emit('message', { topic: roomid, message: value.toString() }, () => {
             console.log("messSent", value.toString());
-
             setIsLoading(false);
-        });}
-        setValue("")
-
+        });
         // socket.timeout(5000).emit('chat message', {roomId : roomid, text : value.toString()}, () => {
         //     setIsLoading(false);
         //     setValue("")
         // });
-
     }
 
 
@@ -55,18 +42,6 @@ const Chatbox = ({ roomid, height }: ChatboxProps) => {
         function onDisconnect() {
             setIsConnected(false);
         }
-        if (paperHeight != undefined && listHeight != undefined && CBheight.current && submissionBox.current) {
-
-            setpaperHeight(CBheight?.current?.clientHeight)
-            console.log(paperHeight);
-
-            setlistHeight(submissionBox?.current?.clientHeight)
-            console.log(listHeight);
-
-
-            setmsgHeight(paperHeight - (listHeight * 2.3));
-        }
-
 
         if (!emitted) {
             socket.emit('subscribe', roomid, () => {
@@ -82,11 +57,6 @@ const Chatbox = ({ roomid, height }: ChatboxProps) => {
             console.log("messReceived", Emessaage);
             if (Emessaage.topic === roomid) {
                 setEmessaages(() => [...Emessaages, Emessaage.message]);}
-
-            
-            if (CBheight.current) {
-                CBheight.current.scrollTop = CBheight.current.scrollHeight - CBheight.current.clientHeight;
-            }
         });
         // socket.on("room#"+roomid, (Emessaage) => {
         //     setEmessaages(() => [...Emessaages, Emessaage.text]);
@@ -97,6 +67,9 @@ const Chatbox = ({ roomid, height }: ChatboxProps) => {
         // function onFooEvent(message: string) {
         //   setFooEvents((prevMessages) => [...prevMessages, message]);
         // }
+
+
+
 
         socket.on('connect', onConnect);
         socket.on('disconnect', onDisconnect);
@@ -110,19 +83,19 @@ const Chatbox = ({ roomid, height }: ChatboxProps) => {
     }, [Emessaages]);
     return (
         <div className="Chatbox">
-            <Paper ref={CBheight} elevation={8} sx={{ position: "relative", minHeight: height }} >
-                <Typography variant="h2" padding={"4%"} textAlign="center">Room #{roomid}</Typography>
-                <Box display="flex" flexDirection="column" justifyContent="space-between" >
-                    <Box sx={{ marginTop: "-3.8%", height: msgHeight, overflowY: "auto" ,backgroundColor:"#f7fbff" }}>
+            <Paper elevation={8} sx={{ position: "relative", minHeight: height }} >
+                <Typography variant="h2" padding={"4%"} textAlign="center">Chat - Room #{roomid}</Typography>
+                <Box display="flex" flexDirection="column" justifyContent="space-between" sx={{ height: "100%" }}>
+                    <Box sx={{ height: window.innerHeight / 2.8, overflowY: "auto" }}>
                         <List >
                             {Emessaages.map((emessage, index) => (
-                                <ListItem ref={messageElm} key={index}> <Typography>{emessage}</Typography> </ListItem>
+                                <ListItem key={index}> <Typography>{emessage}</Typography> </ListItem>
                             ))}
 
                         </List>
                     </Box>
 
-                    <Box ref={submissionBox} sx={{ position: "absolute", bottom: 0, width: "100%" }}>
+                    <Box sx={{ position: "absolute", bottom: 0, width: "100%" }}>
                         <form onSubmit={onSend}>
                             <Grid direction="row"
                                 justifyContent="center"
